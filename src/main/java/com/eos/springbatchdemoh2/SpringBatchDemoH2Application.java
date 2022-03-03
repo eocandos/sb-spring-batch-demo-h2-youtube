1c35e85e66b07bee73c7f3b858a5837f01622b90
Erick <eaocandos@gmail.com>
2022-03-03 11:13:38 -0500

    Empty project SB with all libraries and running

---
+++ src/main/java/com/eos/springbatchdemoh2/SpringBatchDemoH2Application.java
@@ -0,0 +1,13 @@
+package com.eos.springbatchdemoh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDemoH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchDemoH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eos/springbatchdemoh2/SpringBatchDemoH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eos.springbatchdemoh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
